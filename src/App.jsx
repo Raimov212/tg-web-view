e9f868f82d0f9de8d4796afcb731b22b961f2bf6
Raimov212 <jraimov212@gmail.com>
2023-10-26 15:15:23 +0500

    s

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import React, { useEffect } from "react";
 import { useTelegram } from "./hooks/useTelegram";
 import Header from "./components/Header/Header";
 import Form from "./components/Form/FOrm";
-import { Route } from "react-router";
+import { Route, Routes } from "react-router";
+import ProductList from "./components/ProductList/ProductList";
 
 const tg = window.Telegram.WebApp;
 
@@ -14,14 +15,15 @@ const App = () => {
   }, []);
 
   return (
-    <div>
+    <Routes>
       <div className="flex justify">
         <Header />
         <button onClick={onToggleButton}>Toggle</button>
       </div>
       <Route index element={<Form />} />
-      <Route path="/list" element={<Form />} />
-    </div>
+      <Route path="/list" element={<ProductList />} />
+      <Route path="/*" element={<Form />} />
+    </Routes>
   );
 };
 
