aadcbaaf61121a36d2c50ea4b056ed7bad314bf9
Raimov212 <jraimov212@gmail.com>
2023-10-26 13:33:50 +0500

    s

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useTelegram } from "../../hooks/useTelegram";
+
+const Header = () => {
+  const { onClose, user } = useTelegram();
+  return (
+    <div>
+      <h5>{user?.username}</h5>
+      <button onClick={onClose}>Close</button>
+    </div>
+  );
+};
+
+export default Header;
